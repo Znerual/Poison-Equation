4d03fa2435ea607b39d89efac2ac196c17c86084
Laurenz <laurenz_ruzicka@gmx.at>
2020-04-27 09:49:20 +0200

    Projektdateien hinzufügen.

---
+++ Poison Equation.f90
@@ -0,0 +1,25 @@
+!  PoisonEquation.f90 
+!
+!  FUNCTIONS:
+!  PoisonEquation - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: PoisonEquation
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program PoisonEquation
+
+    implicit none
+
+    ! Variables
+
+    ! Body of PoisonEquation
+    print *, 'Hello World'
+
+    end program PoisonEquation
+
